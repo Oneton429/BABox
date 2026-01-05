6cfeff00807e0f94f3b8b058e0160ffe395f0d00
Oneton <56191014+Oneton429@users.noreply.github.com>
2026-01-05 22:18:51 +0800

    打包Python运行环境

--- agent/main.py
+++ agent/main.py
@@ -1,7 +1,19 @@
+import os
 import sys
+from pathlib import Path
 
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
+
+current_file_path = Path(__file__).resolve()
+current_script_dir = current_file_path.parent
+project_root_dir = current_script_dir.parent
+
+if Path.cwd() != project_root_dir:
+    os.chdir(project_root_dir)
+
+if current_script_dir.__str__() not in sys.path:
+    sys.path.insert(0, current_script_dir.__str__())
 
 import action_check_and_stop_student_info_task
 import action_clear_student_info_cache
--- tools/install.py
+++ tools/install.py
@@ -127,6 +127,24 @@ def install_agent():
         dirs_exist_ok=True,
     )
 
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = jsonc.load(f)
+
+    if os_name == "win":
+        interface["agent"]["child_exec"] = r"python/python.exe"
+    elif os_name == "macos":
+        interface["agent"]["child_exec"] = r"python/bin/python3"
+    elif os_name == "linux":
+        interface["agent"]["child_exec"] = r".venv/bin/python3"
+    else:
+        print(f"Unsupported OS: {os_name}")
+        sys.exit(1)
+
+    interface["agent"]["child_args"] = ["-u", r"agent/main.py"]
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        jsonc.dump(interface, f, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     install_deps()
